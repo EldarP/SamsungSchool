1b0984b656f46a4f5b2b3e779d1a487d3daabb2e
EldarP <phonesamnewa@gmail.com>
2022-09-28 14:18:39 +0300

    Create Sum.java

---
+++ untitled/src/Sum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long a, b;
+        a = in.nextLong();
+        b = in.nextLong();
+        System.out.println(a + b);
+        in.close();
+
+    }
+
+}
