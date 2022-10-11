24646985d970908939408ff2d8fd5719fe6f8394
EldarP <phonesamnewa@gmail.com>
2022-10-11 17:27:25 +0300

    Create Task62.java

---
+++ Task1.6-2/src/Task62.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class Task62 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.useLocale(Locale.ENGLISH);
+
+        long x = in.nextLong();
+        long b = in.nextLong();
+        long c = in.nextLong();
+
+        if (x < c && x < b){
+            if (b < c) System.out.println(x + " " + b + " " + c);
+            else System.out.println(x + " " + c + " " + b);
+        }
+
+        if (c < x && c < b){
+            if (x < b) System.out.println(c + " " + x + " " + b);
+            else System.out.println(c + " " + b + " " + x);
+        }
+
+        if (b < x && b < c){
+            if (x < c) System.out.println(b + " " + x + " " + c);
+            else System.out.println(b + " " + c + " " + x);
+        }
+
+    }
+}
